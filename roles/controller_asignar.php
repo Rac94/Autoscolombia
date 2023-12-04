90d6b328cc9549746c57e4d8d2281a97f1c1b37e
Rac94 <134994004+Rac94@users.noreply.github.com>
2023-12-03 19:59:00 -0500

    Proyecto de autocolombia

---
+++ roles/controller_asignar.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ROBIN
+ * Date: 20/11/2023
+ * Time: 22:26
+ */
+
+include('../app/config.php');
+
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$id_user = $_POST['id_user'];
+$rol = $_POST['rol'];
+
+
+date_default_timezone_set("America/Bogota");
+$fechaHora = date("Y-m-d h:i:s");
+
+$sentencia = $pdo->prepare("UPDATE tb_usuarios SET
+rol = :rol
+WHERE id = :id");
+
+$sentencia->bindParam(':rol',$rol);
+$sentencia->bindParam(':id',$id_user);
+
+if($sentencia->execute()){
+    echo "Se asigno el rol de manera correcta";
+    ?>
+    <script>location.href = "../roles/asignar.php";</script>
+    <?php
+}else{
+    echo "error al asignar el rol al usuario";
+}
